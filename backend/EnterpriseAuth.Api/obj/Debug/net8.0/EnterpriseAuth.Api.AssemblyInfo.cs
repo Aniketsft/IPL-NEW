62a8b1176c84b223db22b43c48f21ccd2f542c6b
Ket815 <78716969+Ket815@users.noreply.github.com>
2026-03-11 09:30:45 +0400

    feat: complete architecture planning for production sync system

--- backend/EnterpriseAuth.Api/obj/Debug/net8.0/EnterpriseAuth.Api.AssemblyInfo.cs
+++ backend/EnterpriseAuth.Api/obj/Debug/net8.0/EnterpriseAuth.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnterpriseAuth.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bfe76e919932369a43b5899ae623341c4cdc988")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52f0f9dbbf3899f96436811d14a8de79efec26dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnterpriseAuth.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnterpriseAuth.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
