2f450efaea04f0163db6926acecd011f98cdace6
Ket815 <78716969+Ket815@users.noreply.github.com>
2026-03-12 18:42:03 +0400

    scanner

--- backend/EnterpriseAuth.Api/obj/Debug/net8.0/EnterpriseAuth.Api.AssemblyInfo.cs
+++ backend/EnterpriseAuth.Api/obj/Debug/net8.0/EnterpriseAuth.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnterpriseAuth.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62a8b1176c84b223db22b43c48f21ccd2f542c6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f09d9fdb607528d7745126e93555cbcb7cb7345")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnterpriseAuth.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnterpriseAuth.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
