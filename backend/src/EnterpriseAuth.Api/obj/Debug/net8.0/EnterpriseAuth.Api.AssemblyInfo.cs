439ec3685c53e5a0950f19e53d9afb4340162b74
Ket815 <78716969+Ket815@users.noreply.github.com>
2026-07-06 10:48:19 +0400

    Excess scan allocation

--- backend/src/EnterpriseAuth.Api/obj/Debug/net8.0/EnterpriseAuth.Api.AssemblyInfo.cs
+++ backend/src/EnterpriseAuth.Api/obj/Debug/net8.0/EnterpriseAuth.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnterpriseAuth.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edb927c1fae8d0f052bb41acd61f86a17f76012e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d88087a0480d45a269238123d8bfa2b86ac5e272")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnterpriseAuth.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnterpriseAuth.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
